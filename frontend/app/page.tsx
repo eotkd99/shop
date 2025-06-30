fc50dbca19b851eed571ba21a9e1bd01e06af837
김대상 <eotkd99@mnwise.com>
2025-06-30 17:50:04 +0900

    헤더 레이아웃 추가

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,5 +1,4 @@
-import { MainHeader } from "@/components/MainHeader";
-import { MainCategory } from "@/components/MainCategory";
+// app/somepage/page.tsx
 import { MainPanel } from "@/components/MainPanel";
 import { MainGridPanel } from "@/components/MainGridPanel";
 
@@ -7,8 +6,6 @@ export default function SomePage() {
   return (
     <div>
       <div className="bg-white w-full z-50">
-        <MainHeader />
-        <MainCategory />
         <MainPanel />
         <MainGridPanel />
       </div>
